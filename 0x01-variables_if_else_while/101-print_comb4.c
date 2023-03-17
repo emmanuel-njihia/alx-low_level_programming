1b528829d1ecb803e2801ffc1950e2ea7a243e87
Emmanuel Njihia Ndua <emmanuel.nduan@gmail.com>
2023-03-17 12:26:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int main(void)
 	for (d = '0'; d < '9'; d++)
 	{
 
-	for(p = d + 1; p <= '9'; p++)
+	for (p = d + 1; p <= '9'; p++)
 	{
 
 	for (q = p + 1; q <= '9'; q++)
@@ -25,7 +25,7 @@ int main(void)
 	putchar(d);
 	putchar(p);
 	putchar(q);
-	if ( d == '7' && p == '8')
+	if (d == '7' && p == '8')
 	continue;
 	putchar(',');
 	putchar(' ');
