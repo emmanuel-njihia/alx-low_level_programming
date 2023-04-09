631246cd2376769e3c3c6b991d54d89a6afd2b87
Emmanuel Njihia Ndua <emmanuel.nduan@gmail.com>
2023-04-09 01:23:49 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,14 +6,13 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: Always zero
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv);
 
 	return (0);
 }
