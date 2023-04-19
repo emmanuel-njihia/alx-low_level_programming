9394834e7b39d9c942bdc664fd45041091fc54db
Emmanuel Njihia Ndua <emmanuel.nduan@gmail.com>
2023-04-19 02:28:39 -0700

    This file should contain your main function only

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,18 +1,17 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
+* struct op - A struct op.
+* @op: The operator.
+* @f: The associated function.
+*/
 
 typedef struct op
 
 {
 	char *op;
-	 int (*f)(int a, int b);
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -23,3 +22,4 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
