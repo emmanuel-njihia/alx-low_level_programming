6023c06327d09d517c029cc55f8a8b686ad74c66
Emmanuel Ndua <emmanuel.nduan@gmail.com>
2023-06-09 02:20:29 -0700

    Updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,41 +11,41 @@
 int _atoi(char *s)
 
 {
-	int w, n, k, len, p, digit;
+	int i, d, n, len, f, digit;
 
-	w = 0;
+	i = 0;
+	d = 0;
 	n = 0;
-	k = 0;
 	len = 0;
-	p = 0;
+	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (w < len && p == 0)
+	while (i < len && f == 0)
 	{
-		if (s[w] == '-')
-			++n;
+		if (s[i] == '-')
+			++d;
 
-		if (s[w] >= '0' && s[w] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[w] - '0';
-			if (n % 2)
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
-			k = k * 10 + digit;
-			p = 1;
-			if (s[w + 1] < '0' || s[w + 1] > '9')
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			p = 0;
+			f = 0;
 		}
-		p++;
+		i++;
 	}
 
-	if (p == 0)
+	if (f == 0)
 		return (0);
 
-	return (k);
+	return (n);
 }
 
 /**
